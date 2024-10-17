0c97b8f08983e2e4c1831cfb3d2d5b9ac4bebeee
Vivek Sawant <vicky210490@gmail.com>
2024-10-17 14:34:41 +0530

    add mint button

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,6 +7,7 @@ import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
 // Components
 import { RequestAirdrop } from '../../components/RequestAirdrop';
+import { CandyMint } from 'components/CandyMint';
 import pkg from '../../../package.json';
 
 // Store
@@ -31,10 +32,10 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
-        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
-        <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
-          Solana Next
-        </h1>
+          <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
+          <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
+            Solana Next
+          </h1>
         </div>
         <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
           <p>Unleash the full power of blockchain with Solana and Next.js 13.</p>
@@ -50,17 +51,18 @@ export const HomeView: FC = ({ }) => {
         </div>
         <div className="flex flex-col mt-2">
           <RequestAirdrop />
+          <CandyMint />
           <h4 className="md:w-full text-2xl text-slate-300 my-2">
-          {wallet &&
-          <div className="flex flex-row justify-center">
-            <div>
-              {(balance || 0).toLocaleString()}
+            {wallet &&
+              <div className="flex flex-row justify-center">
+                <div>
+                  {(balance || 0).toLocaleString()}
+                </div>
+                <div className='text-slate-600 ml-2'>
+                  SOL
+                </div>
               </div>
-              <div className='text-slate-600 ml-2'>
-                SOL
-              </div>
-          </div>
-          }
+            }
           </h4>
         </div>
       </div>
